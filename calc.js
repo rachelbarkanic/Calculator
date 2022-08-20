b0d7d4ae636e506cb85324170890d0c19e0082b3
Rachel Barkanic <111482011+rachelbarkanic@users.noreply.github.com>
2022-08-20 13:40:10 -0700

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	const tokens = input.split(' ');
+	
+	const mathSymbol = tokens[0];
+	const num1 = Number(tokens[1]);
+	const num2 = Number(tokens[2]);
+
+	if (mathSymbol === "+") {
+		console.log ('addition',num1 + num2)
+	}
+    if (mathSymbol === "-") {
+		console.log ('subtraction' ,num1 - num2)
+	}
+	if (mathSymbol === "*") {
+		console.log ('multiplication' ,num1 * num2)
+	}
+	if (mathSymbol === "/") {
+		console.log ('division' ,num1 / num2)
+	}
+	if (mathSymbol === 'sqrt') {
+		console.log ('square root' ,Math.sqrt(num1))
+	}
+
+
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
